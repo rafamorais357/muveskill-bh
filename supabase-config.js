c410dcc11a419f334cba9fae1793454beb40c524
rafamorais357 <rfmoraisfotografia@gmail.com>
2026-09-10 23:09:35 -0300

    Create supabase-config.js

---
+++ supabase-config.js
@@ -0,0 +1,7 @@
+// Configuração pública do Supabase - MUVESKILL BH
+// Esta chave é pública e pode ser usada no navegador.
+
+window.MUVESKILL_SUPABASE_CONFIG = {
+  url: 'https://powqdttmvnaccbofzuqs.supabase.co',
+  anonKey: 'sb_publishable_CB4a23gZqyFrtJSETujMHQ_EVhJmIot'
+};
